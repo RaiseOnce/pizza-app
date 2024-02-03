c53a373944a2fdc31c03b5acc5ab226414b8683a
Otto Rahn <elias.yunusoff@gmail.com>
2024-02-03 15:56:11 +0500

    router is done

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import { useState } from 'react'
 import './App.scss'
 import Button from './components/Button/Button'
 import Input from './components/Input/Input'
+import { Route, Routes, createBrowserRouter } from 'react-router-dom'
+import Menu from './pages/Menu/Menu'
+import Cart from './pages/Cart/Cart'
+import Error from './pages/Error/Error'
 
 function App() {
-  // const [counter, setCounter] = useState<number>(0)
-
-  // const addCounter = (e: MouseEvent) => {
-  //   console.log(e)
-  // }
-
   return (
     <div>
-      <Button>ss</Button>
-      <Input placeholder="Email" />
+      <div>
+        <a href="/">Меню</a>
+        <a href="/cart">Корзина</a>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
   )
 }
